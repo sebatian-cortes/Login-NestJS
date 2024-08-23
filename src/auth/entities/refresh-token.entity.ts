21caecebb782e2d9de52de6adc9dab4bc6228a81
flaquito16 <sebastian.agudelo0531@gmail.com>
2024-08-23 14:28:41 -0500

    agrego la parte ya con cambio de contraseña

---
+++ src/auth/entities/refresh-token.entity.ts
@@ -0,0 +1,17 @@
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+  } from 'typeorm';
+
+@Entity('RefreshToken')
+export class RefreshToken{
+    // @PrimaryGeneratedColumn()
+    // id_usuario: number;
+
+    // @Column()
+    // token: string;
+
+    // @Column()
+    // expiryDate: Date;
+}
